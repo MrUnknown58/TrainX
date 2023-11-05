8813c046e946fb4e3c9cd99d0170310840a62934
Aditya407hue <aditya2002sjv@gmail.com>
2023-11-05 13:40:15 +0530

    Changes accepted

--- src/screens/components/Navbar.jsx
+++ src/screens/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='w-full bg-white px-6 py-4 flex justify-between items-center border border-b-2'>
+    <nav className='min-w-full bg-white px-6 py-4 flex justify-between items-center border border-b-2'>
         <div className='text-2xl text-black font-bold'>Train<span className='text-[#0578FF] italic'>X</span></div>
         <div className='space-x-6'>
         <a href="#" className='list-none text-gray-600 text-xl'>My bookings</a>
