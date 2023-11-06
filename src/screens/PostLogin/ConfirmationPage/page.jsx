b4dd52dea97f35b3721815e00efc786b4586dda1
Aditya407hue <aditya2002sjv@gmail.com>
2023-11-07 01:18:50 +0530

    Fill bill

---
+++ src/screens/PostLogin/ConfirmationPage/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FinalBill from './components/FinalBill'
+
+const ConfirmationPage = () => {
+  return (
+    <FinalBill/>
+  )
+}
+
+export default ConfirmationPage
