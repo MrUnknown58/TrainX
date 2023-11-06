c6f27b72bb98a8fbc611332440a2f603e352272d
Aditya407hue <aditya2002sjv@gmail.com>
2023-11-07 00:41:14 +0530

    finalBill added

---
+++ src/screens/PostLogin/PaymentConfirmation/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FinalBill from './components/FinalBill'
+
+const PaymentConfirmation = () => {
+  return (
+    <FinalBill/>
+  )
+}
+
+export default PaymentConfirmation
